89a79a2c9f9d4837a467cc65b1d6868c0249a4a0
VericaG <georgievskaverica9@gmail.com>
2025-10-10 15:28:32 +0200

    Change in JwtConstants

--- src/main/java/com/books/readinglist/constants/JwtConstants.java
+++ src/main/java/com/books/readinglist/constants/JwtConstants.java
@@ -5,4 +5,5 @@ public class JwtConstants {
     public static final String SECRET_KEY = "48ba6d7fba8918b534ace788e43a667988be4de4f8069524e4fc1f82e03c64c5";
     public static final Long EXPIRATION_TIME = 864000000L;
     public static final String HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
 }
