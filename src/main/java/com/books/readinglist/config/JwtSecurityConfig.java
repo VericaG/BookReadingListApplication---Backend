34efa185ba3882494d867c442aafa6a26eb1ddfb
VericaG <georgievskaverica9@gmail.com>
2026-02-28 17:38:46 +0100

    Backend completed

--- src/main/java/com/books/readinglist/config/JwtSecurityConfig.java
+++ src/main/java/com/books/readinglist/config/JwtSecurityConfig.java
@@ -27,7 +27,7 @@ public class JwtSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3001"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
